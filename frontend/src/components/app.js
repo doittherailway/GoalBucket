b3e1b32fd664dc68dd0c3c27043a1796bc476fa8
keyangsun <keyangsun@berkeley.edu>
2019-05-13 11:44:08 -0700

    Add create goal form and container

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -9,7 +9,7 @@ import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import ProfileContainer from './profile/profile_container';
-import TweetComposeContainer from './tweets/tweet_compose_container';
+import CreateGoalFormContainer from './goal_form/goal_form_container'; 
 
 const App = (props) => (
     <div>
@@ -20,7 +20,7 @@ const App = (props) => (
             <AuthRoute exact path="/" component={MainPage} />
             <ProtectedRoute exact path="/goals" component={GoalsContainer} />
             <ProtectedRoute exact path="/profile" component={ProfileContainer} />
-            <ProtectedRoute exact path="/new_tweet" component={TweetComposeContainer} />
+            <ProtectedRoute exact path="/new_goal" component={CreateGoalFormContainer} />
         </Switch>
     </div>
 );
