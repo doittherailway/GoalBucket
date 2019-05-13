6ab0f8a8c4a67efb14e8dc562ab3c7a9fa19986c
Priya Balasubramanian <priya.balasubramanian7@gmail.com>
2019-05-13 10:50:42 -0700

    Add route for Goals container

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,7 +3,8 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import NavBarContainer from './nav/navbar_container';
-import TweetsContainer from './tweets/tweets_container';
+// import TweetsContainer from './tweets/tweets_container';
+import GoalsContainer from './goals/goals_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
@@ -17,7 +18,7 @@ const App = (props) => (
         <NavBarContainer />
         <Switch>
             <AuthRoute exact path="/" component={MainPage} />
-            <ProtectedRoute exact path="/tweets" component={TweetsContainer} />
+            <ProtectedRoute exact path="/goals" component={GoalsContainer} />
             <ProtectedRoute exact path="/profile" component={ProfileContainer} />
             <ProtectedRoute exact path="/new_tweet" component={TweetComposeContainer} />
         </Switch>
