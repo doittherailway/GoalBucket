66378ee1a0f6e5c775bac7994484390675d4fed2
keyangsun <keyangsun@berkeley.edu>
2019-05-13 10:57:10 -0700

    Add goals reducers

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -3,10 +3,12 @@ import session from './session_api_reducer';
 import errors from './errors_reducer';
 import tweets from './tweets_reducer';
 import ui from './ui_reducer';
+import goals from './goals_reducer'; 
 
 const RootReducer = combineReducers({
     session,
     errors,
+    goals,
     tweets,
     ui
 });
