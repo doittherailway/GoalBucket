526f7d57534ce11e8cc7249043328e977b685d18
Jay Swanson <jrswanson020193@gmail.com>
2019-05-13 11:02:41 -0700

    Remove tweets

--- frontend/src/components/profile/profile_container.js
+++ frontend/src/components/profile/profile_container.js
@@ -1,17 +1,14 @@
 import { connect } from 'react-redux';
-import { fetchUserTweets } from '../../actions/tweet_actions';
 import Profile from './profile';
 
 const mapStateToProps = (state) => {
     return {
-        tweets: Object.values(state.tweets.user),
         currentUser: state.session.user
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        fetchUserTweets: id => dispatch(fetchUserTweets(id))
     };
 };
 
