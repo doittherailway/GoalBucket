ebb1ce340a7ebd72ecdbcd62d47800130bb5288d
Jay Swanson <jrswanson020193@gmail.com>
2019-05-14 10:43:38 -0700

    Add users index

---
+++ frontend/src/reducers/users_reducer.js
@@ -0,0 +1,12 @@
+import { RECEIVE_USERS } from '../actions/user_actions';
+
+const usersReducer = (state = [], action) => {
+    switch (action.type) {
+        case RECEIVE_USERS:
+            return action.users
+        default:
+            return state;
+    }
+}
+
+export default usersReducer;
