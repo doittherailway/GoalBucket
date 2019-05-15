d0470130e764831f58c3b1e4a49005e368d08257
railway <doittherailway@gmail.com>
2019-05-15 09:42:20 -0700

    Add username to profile page

--- frontend/src/components/profile/profile_container.js
+++ frontend/src/components/profile/profile_container.js
@@ -4,9 +4,11 @@ import Profile from './profile';
 
 const mapStateToProps = (state) => {
     let selectedGoals = selectUserGoals(state, state.session.user.id);
+    let user = state.entities.users[state.session.user.id];
     return ({
         currentUser: state.session.user,
-        goals: selectedGoals
+        goals: selectedGoals,
+        user: user
     });
 };
 
