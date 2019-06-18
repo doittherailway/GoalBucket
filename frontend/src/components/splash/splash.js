ed357e0198dbbb2fde9ba102d87f76f31c0f9788
railway <doittherailway@gmail.com>
2019-06-17 19:39:56 -0700

    Add additional text to splash page

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -20,24 +20,30 @@ class Splash extends React.Component {
 
     render() {
         return (
-            <div className="splash">
-                <div className="splash-left">
-                    <img className="splash-img-1" src="/images/splash_1.jpg" alt=""/>
-                    <img className="splash-img-2"src="/images/splash_2.jpg" alt="" />
-                    <img className="splash-img-3" src="/images/splash_3.jpg" alt="" />
-                </div>
-                <div className="splash-right">
-                    <div className="auth-modal">
-                        <p>
-                            <strong onClick={this.handleSignup}>register </strong>
-                            /<strong onClick={this.handleLogin}> sign in</strong>
-                        </p>
+            <div>
+                <div className="splash">
+                    <div className="splash-left">
+                        <img className="splash-img-1" src="/images/splash_1.jpg" alt=""/>
+                        <img className="splash-img-2"src="/images/splash_2.jpg" alt="" />
+                        <img className="splash-img-3" src="/images/splash_3.jpg" alt="" />
                     </div>
-                    <h1 className="splash-logo">GOAL</h1>
-                    <h1 className="splash-logo">BUCKET</h1>
-                    <p>A goal setting site that allows you to set goals, 
-                        make progress on your goals, and cheer on your friends goals. 
-                        You can also show off your trophy case of achieved goals.</p>
+                    <div className="splash-right">
+                        <div className="auth-modal">
+                            <p>
+                                <strong onClick={this.handleSignup}>register </strong>
+                                /<strong onClick={this.handleLogin}> sign in</strong>
+                            </p>
+                        </div>
+                        <h1 className="splash-logo">GOAL</h1>
+                        <h1 className="splash-logo">BUCKET</h1>
+                        <p>Be your best self with Goal Bucket.</p>
+                    </div>
+                </div>
+                <div className="splash-btm">
+                    <p> What is GoalBucket? You can set your goals, 
+                        update your progress, and cheer on your friends to reach their goals. Goal Bucket is 
+                        all about the humble brag and show casing your achievements. Start setting your goals today!
+                    </p>
                 </div>
             </div>
         );
